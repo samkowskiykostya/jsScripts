d0a2d7396b324160affb21799d07715e6ccea984
Me <samkowskiykostya@gmail.com>
2017-09-28 22:43:19 -0400

    solei

---
+++ deSoleiSelect.js
@@ -0,0 +1,26 @@
+function waitForEl(i){
+	// Defining search driteria
+	if (i < 100){
+		if($('#seat-initial td[header=2]:visible').get().length == 0){
+			setTimeout(waitForEl, 10000, i+1);
+			console.log('waiting ' + i);
+		}else{
+			console.log('loaded');
+			var section = $('#seat-initial td[header=2] span').contents().get()[0].nodeValue;
+			var row = $('#seat-initial td[header=3] span').contents().get()[0].nodeValue;
+			var seat = $('#seat-initial td[header=4] span').contents().get()[0].nodeValue;
+			// Defining search criteria. row A-D, 5 seats from center. Change it if you want somethign else
+			if (['A','B','C','D'].includes(row) &&
+					(section == '104' && parseInt(seat) <=6 || section == '102' && parseInt(seat) >=6)
+					){
+				alert('Found!');
+			}else{
+				console.log('bad seat, reloading ' + section + row + seat);
+				$('#prev-1').click();
+				$('#next-0').click();
+				setTimeout(waitForEl, 10000, i+1);
+			}
+		}
+	}
+}
+waitForEl(0);
